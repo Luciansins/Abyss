369bec5f246ef31742be2851f60a93c69edd85f3
Sebastian Almendra <script.seba@gmail.com>
2018-07-31 01:53:35 -0400

    implement Venv

--- push7911.py
+++ push7911.py
@@ -1,101 +1,106 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Push a key combination to a list of Cisco Phones
-import requests
 from queue import Queue
 from threading import Thread
+from time import time, sleep
+import requests
 # To disable the warning "InsecureRequestWarning: Unverified HTTPS request is being made"
 # in requests using urllib3,  it's needed to import the specific instance of the module
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-import time
 
-# phone 7911
-# phone_url = 'http://'+authentication+'10.9.44.26/CGI/Execute'
+# <editor-fold desc="Setting">
 authentication = 'orange7911:12345@'
 phone_url = 'http://' + authentication + '{0}/CGI/Execute'
-# Dictionary containing headers for the request
-headers = {
-    'SoapAction': 'CUCM:DB ver=9.1',
-    'Authorization': 'Basic ',
-    'Content-Type': 'text/xml; charset=utf-8'
-}
-# key_array.append('XML=<CiscoIPPhoneExecute><ExecuteItem URL="Key:Settings"/></CiscoIPPhoneExecute>'
-# key_array.append('XML=<CiscoIPPhoneExecute><ExecuteItem URL="Key:Soft4"/></CiscoIPPhoneExecute>')
+execute_data = 'XML=<CiscoIPPhoneExecute><ExecuteItem URL="Key:{0}"/></CiscoIPPhoneExecute>'
 
 # Keys to be sent
-
 key_array = ['Applications', 'KeyPad3', 'KeyPad4', 'KeyPad5']
-
 unlock = ['KeyPadStar', 'KeyPadStar', 'KeyPadPound']
-
 erase = ['KeyPad2', 'Soft4', 'Soft2']
+# </editor-fold>
 
-execute = 'XML=<CiscoIPPhoneExecute><ExecuteItem URL="Key:{0}"/></CiscoIPPhoneExecute>'
+# <editor-fold desc="Configuration">
+filename = 'ip_7911.txt'
+num_worker_threads = 8
+# </editor-fold>
 
 
-class DownloadWorker(Thread):
-    def __init__(self, queue):
+class MigrationWorker(Thread):
+    def __init__(self, q):
         Thread.__init__(self)
-        self.queue = queue
+        self.q = q
 
     def run(self):
         while True:
             # Get the work from the queue and expand the tuple
-            link = self.queue.get()
-            time.sleep(1)
-            print(link)
-            self.queue.task_done()
+            ip = self.q.get()
+            url = phone_url.format(ip)
+            execute_items(url, key_array)
+            sleep(2)
+            execute_items(url, unlock)
+            sleep(0.2)
+            execute_items(url, erase)
+            print(ip)
+            self.q.task_done()
 
 
-def push(keys):
-    r = requests.post(phone_url, headers=headers, data=keys)
-    response = r.text
-    print(response)
+def execute_items(url, keys):
+    for key in keys:
+        payload = execute_data.format(key)
+        xml_push(url, payload)
+        sleep(0.3)
     return
 
 
-with open('ip_7911.txt') as f:
-    content = f.readlines()
+def xml_push(url, payload):
+    # Dictionary containing headers for the request
+    headers = {
+        'SoapAction': 'CUCM:DB ver=9.1',
+        'Authorization': 'Basic ',
+        'Content-Type': 'text/xml; charset=utf-8'
+    }
+    try:
+        r = requests.post(url, headers=headers, data=payload)
+        print(r.text)
+    except requests.exceptions.HTTPError as eHttp:
+        print("Http Error:", eHttp)
+    except requests.exceptions.ConnectionError as eConnection:
+        print("Error Connecting:", eConnection)
+    except requests.exceptions.Timeout as eTimeout:
+        print("Timeout Error:", eTimeout)
+    except requests.exceptions.RequestException as eRequest:
+        print("OOps: Something Else", eRequest)
+    return
 
-content = [x.strip() for x in content]
 
-ts = time()
-# Create a queue to communicate with the worker threads
-queue = Queue()
-# Create 8 worker threads
-for x in range(8):
-    worker = DownloadWorker(queue)
-    # Setting daemon to True will let the main thread exit even though the workers are blocking
-    worker.daemon = True
-    worker.start()
-# Put the tasks into the queue as a tuple
-for link in content:
-    print('Queueing {}'.format(link))
-    queue.put(link)
-# Causes the main thread to wait for the queue to finish processing all the tasks
-queue.join()
-print('Took {}'.format(time() - ts))
+def get_ips(path):
+    with open(path) as file:
+        lines = file.readlines()
+    return [line.strip() for line in lines]
 
-#
-# for line in content:
-#     phone_url = phone_url.format(line)
-#     for x in key_array:
-#         key = execute.format(x)
-#         push(key)
-#         time.sleep(0.3)
-#
-#     time.sleep(2)
-#
-#     for y in unlock:
-#         key = execute.format(y)
-#         push(key)
-#         time.sleep(0.3)
-#
-#     time.sleep(0.2)
-#
-#     for z in erase:
-#         key = execute.format(z)
-#         push(key)
-#         time.sleep(0.3)
+
+def main():
+    ts = time()
+    ips = get_ips(filename)
+    # Create a queue to communicate with the worker threads
+    q = Queue()
+    # Create 8 worker threads
+    for x in range(num_worker_threads):
+        worker = MigrationWorker(q)
+        # Setting daemon to True will let the main thread exit even though the workers are blocking
+        worker.daemon = True
+        worker.start()
+    # Put the tasks into the queue as a tuple
+    for ip in ips:
+        q.put(ip)
+    # Causes the main thread to wait for the queue to finish processing all the tasks
+    q.join()
+    print('Took {}'.format(time() - ts))
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
